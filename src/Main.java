e3ccb94af0ae8561d31d15e04e8b6570f76042f0
Kamal Dua <mrkamaldua@gmail.com>
2019-06-13 01:40:39 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+       public static void main(String args[])
+       {
+              System.out.println(new Solver().solve());
+       }
+
+}
+
+class Solver
+{
+       public String solve()
+       {
+              return "Result";
+       }
+}
